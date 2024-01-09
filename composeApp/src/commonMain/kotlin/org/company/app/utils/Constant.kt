c5cb7bad81e0910d1874e93fffc9cada0b6b96ce
Khubaib Khan <18.bscs.803@gmail.com>
2024-01-09 18:40:47 +0500

    -Search Filter Implemented (#35)

--- composeApp/src/commonMain/kotlin/org/company/app/utils/Constant.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/utils/Constant.kt
@@ -1,7 +1,7 @@
 package org.company.app.utils
 
 object Constant {
-    const val API_KEY = "AIzaSyCvp3-o7Q8Co3GDf8lDNgb41ZEJ1ZI-TFM"
+    const val API_KEY = "AIzaSyAWbSfonfXkfW5maACt1mOKZYixcIS4sKM"
     const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
     const val TIMEOUT = 300000L
     const val SHORT_URL = "https://www.youtube.com/shorts/"
