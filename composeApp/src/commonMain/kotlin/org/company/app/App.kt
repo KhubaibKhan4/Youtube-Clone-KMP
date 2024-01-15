269065cf3208180a1e46720536d24f4e8c8637af
Khubaib Khan <18.bscs.803@gmail.com>
2024-01-15 11:24:54 +0500

    -Code Ehancement (#43)

--- composeApp/src/commonMain/kotlin/org/company/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/App.kt
@@ -5,10 +5,21 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.mandatorySystemGestures
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -96,7 +107,7 @@ internal fun App() = AppTheme {
         Scaffold(bottomBar = {
             if (!showNavigationRail) {
                 BottomNavigation(
-                    modifier = Modifier.fillMaxWidth().height(69.dp),
+                    modifier = Modifier.fillMaxWidth().windowInsetsPadding(WindowInsets.ime),
                     backgroundColor = MaterialTheme.colorScheme.background,
                     contentColor = contentColorFor(Color.Red),
                     elevation = 8.dp
