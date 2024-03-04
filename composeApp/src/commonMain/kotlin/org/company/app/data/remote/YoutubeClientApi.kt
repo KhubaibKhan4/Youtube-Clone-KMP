d3eca8d5757cee1fd1ee42892bb8252d1c71ceec
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-04 18:02:07 +0500

    -Logger Impl (#148)

--- composeApp/src/commonMain/kotlin/org/company/app/data/remote/YoutubeClientApi.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/remote/YoutubeClientApi.kt
@@ -1,10 +1,10 @@
 package org.company.app.data.remote
 
+import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -33,8 +33,13 @@ object YoutubeClientApi {
         }
 
         install(Logging) {
-            logger = Logger.DEFAULT
             level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Napier.log(priority = io.github.aakira.napier.LogLevel.DEBUG, tag = "LOGS",message = message)
+                }
+
+            }
         }
 
         install(HttpTimeout) {
