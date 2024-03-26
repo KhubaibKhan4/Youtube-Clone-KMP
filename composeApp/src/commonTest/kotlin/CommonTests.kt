b5dc71e017f4c55a458bb19be669cafa1796b4ff
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-26 10:36:39 +0500

    -Common Test cases Init (#211)

---
+++ composeApp/src/commonTest/kotlin/CommonTests.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class CommonTests {
+    @Test
+    fun testUsernameNotEmpty(){
+        val result = "Username"
+        assertTrue(actual = true,message = null)
+    }
+}
