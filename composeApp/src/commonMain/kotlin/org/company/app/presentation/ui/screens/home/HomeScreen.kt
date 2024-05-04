81376285099d557a54c310f73b920c1adfcae8cc
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-04 13:58:32 +0500

    -Pull to refersh Impl (#289)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/home/HomeScreen.kt
@@ -1,10 +1,25 @@
 package org.company.app.presentation.ui.screens.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.company.app.UserRegion
 import org.company.app.domain.usecases.ResultState
 import org.company.app.isConnected
@@ -23,51 +38,78 @@ class HomeScreen() : Screen {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeContent(
     viewModel: MainViewModel = koinInject<MainViewModel>(),
 ) {
-    LaunchedEffect(Unit) {
+    val refreshScope = rememberCoroutineScope()
+    var refreshing by remember { mutableStateOf(false) }
+
+    fun refresh() = refreshScope.launch {
+        refreshing = true
         viewModel.getVideosList(UserRegion())
-        //viewModel.getAllVideos()
+        delay(1500)
+        refreshing = false
     }
-    if (isConnected(retry = {})) {
-        val state by viewModel.videos.collectAsState()
-        when (state) {
-            is ResultState.LOADING -> {
-                ShimmerEffectMain()
-            }
 
-            is ResultState.SUCCESS -> {
-                val data = (state as ResultState.SUCCESS).response
-                VideosList(data)
-            }
+    val pullRefreshState = rememberPullRefreshState(refreshing, ::refresh)
 
-            is ResultState.ERROR -> {
-                val error = (state as ResultState.ERROR).error
-                if (!isConnected(retry = {})) {
-                    NoInternet()
-                } else {
-                    ErrorBox(error)
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .pullRefresh(pullRefreshState),
+        contentAlignment = Alignment.Center
+    ) {
+        if (!refreshing) {
+            if (isConnected(retry = {})) {
+                LaunchedEffect(Unit) {
+                    viewModel.getVideosList(UserRegion())
+                }
+                val state by viewModel.videos.collectAsState()
+                when (state) {
+                    is ResultState.LOADING -> {
+                        ShimmerEffectMain()
+                    }
+
+                    is ResultState.SUCCESS -> {
+                        val data = (state as ResultState.SUCCESS).response
+                        VideosList(data)
+                    }
+
+                    is ResultState.ERROR -> {
+                        val error = (state as ResultState.ERROR).error
+                        if (!isConnected(retry = {})) {
+                            NoInternet()
+                        } else {
+                            ErrorBox(error)
+                        }
+                    }
+                }
+            } else {
+                val localState by viewModel.localVideos.collectAsState()
+                when (localState) {
+                    is ResultState.ERROR -> {
+                        val error = (localState as ResultState.ERROR).error
+                        ErrorBox(error)
+                    }
+
+                    ResultState.LOADING -> {
+                        ShimmerEffectMain()
+                    }
+
+                    is ResultState.SUCCESS -> {
+                        val response = (localState as ResultState.SUCCESS).response
+                        OfflineList(response)
+                    }
                 }
             }
+        }else{
+            ShimmerEffectMain()
         }
-    } else {
-        val localState by viewModel.localVideos.collectAsState()
-        when (localState) {
-            is ResultState.ERROR -> {
-                val error = (localState as ResultState.ERROR).error
-                ErrorBox(error)
-            }
-
-            ResultState.LOADING -> {
-                ShimmerEffectMain()
-            }
-
-            is ResultState.SUCCESS -> {
-                val response = (localState as ResultState.SUCCESS).response
-                OfflineList(response)
-            }
-        }
+        PullRefreshIndicator(
+            refreshing, pullRefreshState,
+            Modifier.wrapContentSize().align(Alignment.TopCenter)
+        )
     }
 }
