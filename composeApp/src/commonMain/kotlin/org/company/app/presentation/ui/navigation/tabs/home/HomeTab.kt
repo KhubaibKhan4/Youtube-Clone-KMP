9a530817d49efa9756ef0855908af54c074d0a0c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-08 09:53:29 +0500

    -Home Navigation Enhancement (#388)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/navigation/tabs/home/HomeTab.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/navigation/tabs/home/HomeTab.kt
@@ -7,15 +7,12 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import cafe.adriel.voyager.transitions.SlideTransition
 import org.company.app.presentation.ui.screens.home.HomeScreen
 
 object HomeTab : Tab {
     @Composable
     override fun Content() {
-        Navigator(HomeScreen()) { navigator ->
-            SlideTransition(navigator)
-        }
+        Navigator(HomeScreen())
     }
 
     override val options: TabOptions
