0b710f83d412c0ba594334e9dc8aaffe7707f7a3
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-15 12:25:44 +0500

    -Detail Content Impl (#424)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/detail/DetailContent.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/detail/DetailContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
