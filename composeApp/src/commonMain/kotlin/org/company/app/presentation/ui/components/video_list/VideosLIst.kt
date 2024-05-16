1c56f0a2297aa790e8c92a24d608ed52d993cd1d
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-16 10:14:40 +0500

    -Video List Enhancement Impl (#425)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/video_list/VideosLIst.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/video_list/VideosLIst.kt
@@ -126,10 +126,7 @@ import youtube_clone.composeapp.generated.resources.youtube_logo_light
 import org.company.app.domain.model.videos.Item as VideoItem
 import org.company.app.presentation.ui.screens.detail.formatViewCount as FormateView
 
-@OptIn(
-    ExperimentalResourceApi::class, ExperimentalMaterial3WindowSizeClassApi::class,
-    ExperimentalAnimationApi::class
-)
+@OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun VideosList(
     youtube: Youtube,
