dfcf50cb8e2f7961249bb94ff7ec9a060d5deaa0
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-23 09:31:09 +0500

    -Compose Compiler Bump (#440)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.libres).apply(false)
     alias(libs.plugins.buildConfig).apply(false)
     alias(libs.plugins.kotlinx.serialization).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
 }
