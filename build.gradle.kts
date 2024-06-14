e92911031b627cd1628c575fafa2514d0ecf96ce
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-14 09:59:54 +0500

    -Libre Cleanup (#477)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.multiplatform).apply(false)
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
-    alias(libs.plugins.libres).apply(false)
     alias(libs.plugins.buildConfig).apply(false)
     alias(libs.plugins.kotlinx.serialization).apply(false)
     alias(libs.plugins.compose.compiler).apply(false)
