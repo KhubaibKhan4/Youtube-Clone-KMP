a0e69eff1a16b6f227426f9d9548004b0d17e6c4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-07-11 15:44:15 +0500

    -Desktop Player Enhancement (#567)

--- composeApp/src/jvmMain/kotlin/org/company/app/DesktopWebView.kt
+++ composeApp/src/jvmMain/kotlin/org/company/app/DesktopWebView.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import javafx.application.Platform
+import javafx.concurrent.Worker
 import javafx.embed.swing.JFXPanel
 import javafx.scene.Scene
 import javafx.scene.web.WebView
@@ -35,30 +36,34 @@ private fun JFXPanel.buildWebView(url: String) {
         val webView = WebView()
         val webEngine = webView.engine
 
-        // Set the user agent to simulate a browser for YouTube
         webEngine.userAgent =
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        val script = """
-    setTimeout(function() {
-        var element = document.querySelector('.ytp-chrome-top.ytp-show-cards-title');
-        if (element !== null) {
-            element.style.display = "none";
-        }
-        // Repeat the above pattern for other elements...
-    }, 1000); // Adjust the timeout value as needed
-""".trimIndent()
 
-
-        webEngine.executeScript(script)
-
-        // Enable JavaScript support for YouTube embed player
         webEngine.isJavaScriptEnabled = true
 
-        // Enable full-screen mode support
-        webEngine.executeScript("document.webkitExitFullscreen = function() {};")
-        // Load the YouTube video using the embed URL
         webEngine.load(url)
+
         val scene = Scene(webView)
         setScene(scene)
+
+        webEngine.loadWorker.stateProperty().addListener { _, _, newState ->
+            if (newState == Worker.State.SUCCEEDED) {
+                val script = """
+                    setTimeout(function() {
+                        var overlaySelectors = [
+                            '.ytp-gradient-top',
+                            '.ytp-gradient-bottom'
+                        ];
+                        overlaySelectors.forEach(function(selector) {
+                            var element = document.querySelector(selector);
+                            if (element !== null) {
+                                element.style.display = 'none';
+                            }
+                        });
+                    }, 1000); // Adjust the timeout value as needed
+                """.trimIndent()
+                webEngine.executeScript(script)
+            }
+        }
     }
 }
