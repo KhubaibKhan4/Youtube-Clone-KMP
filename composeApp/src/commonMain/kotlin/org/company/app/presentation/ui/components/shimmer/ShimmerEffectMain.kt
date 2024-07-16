fa6e05ba802d200585bdb685312c9ef2e5836a1e
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-07-16 11:48:24 +0500

    -Shimmer Effect Enhancement (#585)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/shimmer/ShimmerEffectMain.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/components/shimmer/ShimmerEffectMain.kt
@@ -26,7 +26,9 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,6 +36,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import org.company.app.theme.LocalThemeIsDark
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @Composable
@@ -66,6 +69,7 @@ fun ShimmerEffectMain() {
 fun ShimmerItem(
     brush: Brush
 ) {
+    val isDark by LocalThemeIsDark.current
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Top,
@@ -117,7 +121,14 @@ fun ShimmerItem(
                 )
                 Spacer(modifier = Modifier.width(6.dp))
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = TopAppBarColors(
+                containerColor = if (isDark) Color(0xFF202020) else Color.White,
+                titleContentColor = if (isDark) Color.White else Color.Black,
+                navigationIconContentColor = if (isDark) Color.White else Color.Black,
+                actionIconContentColor = if (isDark) Color.White else Color.Black,
+                scrolledContainerColor = if (isDark) Color.White else Color.Black,
+            )
         )
 
         LazyRow(
@@ -149,7 +160,7 @@ fun ShimmerItem(
                             .fillMaxWidth()
                             .padding(8.dp),
                         colors = CardDefaults.cardColors(
-                            containerColor = MaterialTheme.colorScheme.surface
+                            containerColor =if (isDark) Color(0xFF202020) else Color.White
                         )
                     ) {
                         Column {
