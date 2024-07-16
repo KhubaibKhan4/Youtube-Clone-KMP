e011ecb5cdcffd02477451d969f134ee5620afe9
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-07-16 16:37:43 +0500

    -Updated Banner Impl (#589)

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -4,9 +4,7 @@ import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.FirebaseOptions
 import dev.gitlive.firebase.initialize
 import org.company.app.App
-import org.company.app.di.appModule
 import org.jetbrains.skiko.wasm.onWasmReady
-import org.koin.core.context.startKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
