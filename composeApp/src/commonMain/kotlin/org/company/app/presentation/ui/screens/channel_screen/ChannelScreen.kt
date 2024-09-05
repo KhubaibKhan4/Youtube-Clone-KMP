ffc64342a954901d53524907ff9a1c1be52f1f26
Muhammad Khubaib Imtiaz <98816544+KhubaibKhan4@users.noreply.github.com>
2024-09-05 11:27:34 +0500

    Channal Screen Ui Enhance

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/channel_screen/ChannelScreen.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/channel_screen/ChannelScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight  
 import androidx.compose.foundation.layout.fillMaxSize
