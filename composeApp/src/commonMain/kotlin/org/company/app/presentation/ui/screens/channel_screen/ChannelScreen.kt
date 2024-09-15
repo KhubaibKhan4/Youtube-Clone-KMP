1fab767c13a02dbdb44d77cc4d0fdcfad55ef277
Muhammad Khubaib Imtiaz <rabeetahmad@Muhammad-Khubaib-Imtiaz.local>
2024-09-15 23:02:58 +0500

    -Channel Screen Enhancement

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/channel_screen/ChannelScreen.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/ui/screens/channel_screen/ChannelScreen.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight  
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size 
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -300,7 +303,7 @@ fun ChannelContent(
         
         Column(
             modifier = Modifier.fillMaxSize()
-                .padding(top = 49.dp),
+                .padding(top = 65.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
@@ -400,7 +403,8 @@ fun ChannelContent(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 25.dp)
+                .padding(top = 65.dp)
+                .windowInsetsPadding(WindowInsets.navigationBars)
                 .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
