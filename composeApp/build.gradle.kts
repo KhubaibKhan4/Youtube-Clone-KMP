60ec560123f6cc98574c9989a5e15eee6fdfe9f4
KhubaibKhan4 <18.bscs.803@gmail.com>
2024-09-24 21:42:35 +0500

    -MediaPlayer Bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -68,7 +68,7 @@ kotlin {
             implementation("org.jetbrains.androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
             implementation("net.thauvin.erik.urlencoder:urlencoder-lib:1.5.0")
             implementation(libs.alert.kmp)
-            implementation("io.github.khubaibkhan4:mediaplayer-kmp:1.1.2")
+            implementation("io.github.khubaibkhan4:mediaplayer-kmp:1.1.3")
             implementation(libs.gitlive.firebase.firestore)
             implementation(libs.gitlive.firebase.database)
         }
