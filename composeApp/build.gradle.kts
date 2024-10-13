f3ce11d6363199bd12da9a177acdc428b31660fa
KhubaibKhan4 <18.bscs.803@gmail.com>
2024-10-13 20:15:21 +0500

    -JavaFx Bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
     alias(libs.plugins.sqlDelight)
     alias(libs.plugins.libres)
     alias(libs.plugins.compose.compiler)
-    id("com.google.osdetector") version "1.7.3"
     id("com.google.gms.google-services")
 }
 kotlin {
@@ -110,20 +109,6 @@ kotlin {
             implementation(compose.desktop.common)
             implementation(compose.desktop.currentOs)
             implementation(libs.ktor.client.okhttp)
-            val fxSuffix = when (osdetector.classifier) {
-                "linux-x86_64" -> "linux"
-                "linux-aarch_64" -> "linux-aarch64"
-                "windows-x86_64" -> "win"
-                "osx-x86_64" -> "mac"
-                "osx-aarch_64" -> "mac-aarch64"
-                else -> throw IllegalStateException("Unknown OS: ${osdetector.classifier}")
-            }
-            implementation("org.openjfx:javafx-base:19:${fxSuffix}")
-            implementation("org.openjfx:javafx-graphics:19:${fxSuffix}")
-            implementation("org.openjfx:javafx-controls:19:${fxSuffix}")
-            implementation("org.openjfx:javafx-swing:19:${fxSuffix}")
-            implementation("org.openjfx:javafx-web:19:${fxSuffix}")
-            implementation("org.openjfx:javafx-media:19:${fxSuffix}")
             implementation(libs.sqlDelight.driver.sqlite)
             implementation(libs.kotlinx.coroutines.swing)
         }
